7a5ae0f65520c992abcd21e6bd5c265fc303564b
Ahmad <ahmaddyab12345@gmial.com>
2025-05-22 12:19:56 +0300

    Fix: حل الأخطاء وتجهيز للنشر

--- src/app/[locale]/Components/Bar/page.tsx
+++ src/app/[locale]/Components/Bar/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from 'react'
+import React from "react";
 import Image from "next/image";
 import { IoArrowBackCircleOutline, IoSettingsOutline } from "react-icons/io5";
 import { MdLogout } from "react-icons/md";
@@ -8,22 +8,26 @@ import { PiUsersBold } from "react-icons/pi";
 import { RiHome5Fill } from "react-icons/ri";
 import { TfiViewList } from "react-icons/tfi";
 import { IoIosArrowDropdown } from "react-icons/io";
-import "./Bar.css"
-import { useRouter } from 'next/navigation';
-import { useLocale } from 'next-intl';
-
-function Bar({ active, setActive, setSection }) {
-    const locale = useLocale();
+import "./Bar.css";
+import { useRouter } from "next/navigation";
+import { useLocale } from "next-intl";
+type BarProps = {
+  active: boolean;
+  setActive: (value: boolean) => void;
+  setSection: (value: string) => void;
+};
+function Bar({ active, setActive, setSection }: BarProps) {
+  const locale = useLocale();
   const router = useRouter();
 
   const handleLogout = async () => {
     const refreshToken = localStorage.getItem("refresh");
-  
+
     if (!refreshToken) {
       console.error("No refresh token found!");
       return;
     }
-  
+
     try {
       const response = await fetch("http://46.202.135.90:81/en/api/logout/", {
         method: "POST",
@@ -32,7 +36,7 @@ function Bar({ active, setActive, setSection }) {
         },
         body: JSON.stringify({ refresh: refreshToken }),
       });
-  
+
       if (response.ok) {
         console.log("تم تسجيل الخروج بنجاح");
       } else {
@@ -46,13 +50,14 @@ function Bar({ active, setActive, setSection }) {
       router.push(`/${locale}/Login`); // رجع المستخدم على صفحة الدخول
     }
   };
-  
 
   return (
     <div className={`bar ${active ? "active" : ""}`}>
       <div className="flex justify-between">
         <h1>Logo</h1>
-        {active && <IoArrowBackCircleOutline onClick={() => setActive(false)} />}
+        {active && (
+          <IoArrowBackCircleOutline onClick={() => setActive(false)} />
+        )}
       </div>
       <ul>
         <li className="reefi">
@@ -65,7 +70,8 @@ function Bar({ active, setActive, setSection }) {
           <RiHome5Fill style={{ color: "#3981F7" }} /> Dashboard
         </li>
         <li onClick={() => setSection("Order Analysis")}>
-          <Image src={"/message.png"} width={"30"} height={"30"} alt="" /> Order Analysis
+          <Image src={"/message.png"} width={"30"} height={"30"} alt="" /> Order
+          Analysis
         </li>
         <li onClick={() => setSection("Product")}>
           <TfiViewList style={{ color: "#904DFF" }} /> Product
@@ -74,25 +80,32 @@ function Bar({ active, setActive, setSection }) {
           <PiUsersBold style={{ color: "#05FF00" }} /> Customers
         </li>
         <li onClick={() => setSection("Coupons")}>
-          <Image src={"/coupons.png"} width={"30"} height={"30"} alt="" /> Coupons
+          <Image src={"/coupons.png"} width={"30"} height={"30"} alt="" />{" "}
+          Coupons
         </li>
         <li onClick={() => setSection("Countries")}>
-          <Image src={"/Countries.png"} width={"30"} height={"30"} alt="" /> Countries
+          <Image src={"/Countries.png"} width={"30"} height={"30"} alt="" />{" "}
+          Countries
         </li>
         <li onClick={() => setSection("Payments")}>
-          <Image src={"/Payments.png"} width={"30"} height={"30"} alt="" /> Payments
+          <Image src={"/Payments.png"} width={"30"} height={"30"} alt="" />{" "}
+          Payments
         </li>
         <li onClick={() => setSection("Shipping")}>
-          <Image src={"/Shipping.png"} width={"30"} height={"30"} alt="" /> Shipping
+          <Image src={"/Shipping.png"} width={"30"} height={"30"} alt="" />{" "}
+          Shipping
         </li>
         <li onClick={() => setSection("Delivery tool indicator")}>
-          <Image src={"/Delivery.png"} width={"30"} height={"30"} alt="" /> Delivery tool indicator
+          <Image src={"/Delivery.png"} width={"30"} height={"30"} alt="" />{" "}
+          Delivery tool indicator
         </li>
         <li onClick={() => setSection("Retarget")}>
-          <Image src={"/Retarget.png"} width={"30"} height={"30"} alt="" /> Retarget
+          <Image src={"/Retarget.png"} width={"30"} height={"30"} alt="" />{" "}
+          Retarget
         </li>
         <li onClick={() => setSection("Data bank")}>
-          <Image src={"/Data.png"} width={"30"} height={"30"} alt="" /> Data bank
+          <Image src={"/Data.png"} width={"30"} height={"30"} alt="" /> Data
+          bank
         </li>
         <li onClick={() => setSection("Settings")}>
           <IoSettingsOutline style={{ color: "#904DFF" }} /> Settings
@@ -106,5 +119,3 @@ function Bar({ active, setActive, setSection }) {
 }
 
 export default Bar;
-
-
